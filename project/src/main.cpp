872d0f604f9b1d7a0514b873d83950bf15ba8485
aarav9291 <aaravagarwal9291@gmail.com>
2026-06-05 22:27:54 +0530

    Update main.cpp
    Refactor main function to use Strategy pattern and remove unnecessary output.

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,28 +1,24 @@
 #include "engine.hpp"
-#include <iostream>
+#include "strategy.hpp"
+
+#include <memory>
 
 using namespace std;
 
-int main(int argc, char* argv[]){
+extern "C" csot::Strategy* create_strategy();
+
+int main(int argc, char* argv[])
+
+
     Engine engine;
+
     auto ticks = engine.load_ticks(argv[1]);
-    cout << ticks.size() << '\n';
-    cout << "First tick:\n";
-    cout << ticks.front().timestamp_ns << " "
-         << ticks.front().symbol << " "
-         << ticks.front().bid_px << " "
-         << ticks.front().ask_px << " "
-         << ticks.front().bid_qty << " "
-         << ticks.front().ask_qty << '\n';
-    cout << "Last tick:\n";
-    cout << ticks.back().timestamp_ns << " "
-         << ticks.back().symbol << " "
-         << ticks.back().bid_px << " "
-         << ticks.back().ask_px << " "
-         << ticks.back().bid_qty << " "
-         << ticks.back().ask_qty << '\n';
-    cout << "ticks[0].symbol = "
-         << ticks[0].symbol
-         << '\n';
+
+    unique_ptr<csot::Strategy> strategy(create_strategy());
+
+    strategy->on_init();
+
+    engine.run(ticks, *strategy);
+
     return 0;
 }
