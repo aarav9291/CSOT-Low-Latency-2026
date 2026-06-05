9f910c0666721d562d9a7f66365a82ce11ef3e8d
aarav9291 <aaravagarwal9291@gmail.com>
2026-06-05 20:56:25 +0530

    Update spec_strategy.cpp

--- project/strategies/spec_strategy.cpp
+++ project/strategies/spec_strategy.cpp
@@ -1,1 +1,15 @@
+#include "strategy.hpp"
+#include <vector>
 
+using namespace std;
+
+class NullStrategy : public csot::Strategy {
+public:
+    vector<csot::Order> on_tick(const csot::Tick&) override {
+        return {};
+    }
+};
+
+extern "C" csot::Strategy* create_strategy() {
+    return new NullStrategy();
+}
