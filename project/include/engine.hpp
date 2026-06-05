a0bf78a97c26a51336ab091d70ef4fae84f85465
aarav9291 <aaravagarwal9291@gmail.com>
2026-06-05 20:14:05 +0530

    Update engine.hpp

--- project/include/engine.hpp
+++ project/include/engine.hpp
@@ -1,1 +1,18 @@
+#pragma once
+#include "strategy.hpp"
+#include <deque>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <vector>
 
+using namespace std;
+
+class Engine {
+public:
+    vector<csot::Tick> load_ticks(const string& path);
+
+private:
+    deque<string> symbol_storage_;
+    unordered_map<string, string_view> interned_;
+};
