98d9c27e179f1eec092566ccb91e343ca90e5c2f
aarav9291 <aaravagarwal9291@gmail.com>
2026-06-05 21:29:07 +0530

    Update engine.hpp
    Removed unused includes and class definition, added replay_ticks function.

--- project/include/engine.hpp
+++ project/include/engine.hpp
@@ -1,18 +1,10 @@
 #pragma once
 #include "strategy.hpp"
-#include <deque>
 #include <string>
-#include <string_view>
-#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-class Engine {
-public:
-    vector<csot::Tick> load_ticks(const string& path);
+vector<csot::Tick> load_ticks(const string& path);
 
-private:
-    deque<string> symbol_storage_;
-    unordered_map<string, string_view> interned_;
-};
+void replay_ticks(const vector<csot::Tick>& ticks,csot::Strategy& strategy);
