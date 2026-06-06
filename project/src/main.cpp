876b54a8e1a91c9f16d56f72e7aa9a3a4d644cdc
aarav9291 <aaravagarwal9291@gmail.com>
2026-06-07 01:00:33 +0530

    Update main.cpp

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,29 +1,17 @@
 #include "engine.hpp"
+#include "strategy.hpp"
 
-#include <iostream>
+#include <memory>
 
 using namespace std;
+
+extern "C" csot::Strategy* create_strategy();
 
 int main(int argc, char* argv[]){
     Engine engine;
     auto ticks = engine.load_ticks(argv[1]);
-    cout << ticks.size() << '\n';
-    cout << "First tick:\n";
-    cout << ticks.front().timestamp_ns << " "
-         << ticks.front().symbol << " "
-         << ticks.front().bid_px << " "
-         << ticks.front().ask_px << " "
-         << ticks.front().bid_qty << " "
-         << ticks.front().ask_qty << '\n';
-    cout << "Last tick:\n";
-    cout << ticks.back().timestamp_ns << " "
-         << ticks.back().symbol << " "
-         << ticks.back().bid_px << " "
-         << ticks.back().ask_px << " "
-         << ticks.back().bid_qty << " "
-         << ticks.back().ask_qty << '\n';
-    cout << "ticks[0].symbol = "
-         << ticks[0].symbol
-         << '\n';
+    unique_ptr<csot::Strategy> strategy(create_strategy());
+    strategy->on_init();
+    engine.tick_loop(ticks,*strategy);
     return 0;
 }
